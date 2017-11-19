3fd2b3dc0d2878954a093d2a59e903c77bc9dc7e
DESKTOP-QQ4UMN4\asus <Pongsatorn698@gmail.com>
2017-11-19 11:39:33 +0700

    Puss 1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Campe.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
